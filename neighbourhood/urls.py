7f0f87959f56759ccaf2ec03d98db7a5cc405cd0
Antony <amwaniki180@gmail.com>
2019-08-09 15:57:21 +0300

    added routing

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -3,4 +3,18 @@ from . import views
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
+    url(r'^profile',views.profile, name= 'profile'),
+    url(r'^update',views.update_profile, name= 'update'),
+    url(r'^update',views.create_profile, name= 'create'),
+    url(r'^new_post',views.new_post ,name= 'new_post'),
+    url(r'^add/neighbourhood',views.new_project, name= 'add_project'),
+    url(r'^new_business',views.new_business ,name= 'new_business'),
+    url(r'^project/(\d+)', views.project, name = "project"),
+    url(r'^post/(\d+)', views.post, name = "post"),
+    url(r'^new_post',views.new_post ,name= 'new_post'),
+    url(r'^search/', views.search, name='search'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
