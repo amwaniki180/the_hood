b99ac65a348d132730452ccfc4085d864f09ed6a
Antony <amwaniki180@gmail.com>
2019-08-09 15:54:20 +0300

    forms.py

---
+++ neighbourhood/forms.py
@@ -0,0 +1,26 @@
+rom django import forms
+from django.contrib.auth.models import User
+from django.core import validators
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from .models import Profile,Business,Project,Post
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude=['user']
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['owner']
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude=['admin']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude=['poster']
+            
