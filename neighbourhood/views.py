9318570d5a561d47a235ce99e4995cb08d6b5933
Antony <amwaniki180@gmail.com>
2019-08-09 15:39:53 +0300

    search.html

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -68,3 +68,102 @@ def update_profile(request):
     
     return render(request,'update_profile.html',{"form":form})
 
+@login_required(login_url='/accounts/login/')
+def new_hood(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NeighbourhoodForm(request.POST,request.FILES)
+        if form.is_valid():
+            neighbourhood = form.save(commit=False)
+            neighbourhood.hood_admin= current_user
+            neighbourhood.save()
+        return redirect('home')
+    else:
+        form = NeighbourhoodForm()
+    
+    return render(request,'new_hood.html',{"form":form})
+
+@login_required(login_url='/accounts/login/')
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = PostForm(request.POST,request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.poster = current_user
+            post.save()
+        return redirect('home')
+    else:
+        form = PostForm()
+    
+    return render(request,'new_post.html',{"form":form})
+
+@login_required(login_url='/accounts/login/')
+def new_business(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = BusinessForm(request.POST,request.FILES)
+        if form.is_valid():
+            business = form.save(commit=False)
+            business.owner = current_user
+            business.save()
+        return redirect('home')
+    else:
+        form = BusinessForm()
+    
+    return render(request,'new_business.html',{"form":form})
+
+@login_required(login_url='/accounts/login/')
+def project(request,project_id):
+    try:
+        project = Project.objects.get(id = project_id)
+
+    except DoesNotExist:
+        raise Http404()
+
+    business = Business.get_business(project_id)  
+    posts = Post.get_post(project_id)
+
+    # is_joined = False
+    # if project.join.filter(id = request.user.id).exists():
+    #     is_joined = True
+
+    return render(request, 'project.html', {"project": project,"business":business,"posts":posts})
+
+@login_required(login_url='/accounts/login/')
+def post(request,post_id):
+    try:
+        post = Post.objects.get(id = post_id)
+
+    except DoesNotExist:
+        raise Http404()
+
+    
+    return render(request, 'post.html', {"post":post})
+
+# @login_required(login_url='/accounts/login/')
+# def new_post(request):
+#     current_user = request.user
+#     if request.method == 'POST':
+#         form = PostForm(request.POST,request.FILES)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.poster = current_user
+#             post.save()
+#         return redirect('home')
+#     else:
+#         form = PostForm()
+    
+def search(request):
+    if 'search' in request.GET and request.GET['search']:
+        search_term = request.GET.get('search')
+        business = Business.search_business(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html',{'message':message, 'business':business})
+    else:
+        message = 'Enter term to search'
+        return render(request, 'search.html', {'message':message})
+        
+
+
